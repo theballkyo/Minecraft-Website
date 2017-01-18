f5ec162c52362b508eebcccaa66fb6872f57c9a7
Theballkyo <theballkyo@gmail.com>
2017-01-18 10:54:31 +0700

    Revert "Merged branch dev into feature/markdown-editor"
    This reverts commit 6756499540a8f40d5620ef5203aba65c972448de.

--- src/app/Http/Controllers/BoardController.php
+++ src/app/Http/Controllers/BoardController.php
@@ -13,8 +13,7 @@ use Illuminate\Support\Facades\Cache;
 
 class BoardController extends Controller
 {
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('auth', ['except' => [
             'index',
             'show',
@@ -23,14 +22,13 @@ class BoardController extends Controller
 
     /**
      * Show all topics
-     * @param  Request $request
+     * @param  Request  $request
      *
      * @return String view
      */
-    public function index(Request $request)
-    {
-        $data = Cache::remember('topic.all.' . $request->cat, 1, function () use ($request) {
-            $cat = (int)$request->cat;
+    public function index(Request $request) {
+        $view = Cache::remember('topic.all.' . $request->cat, 1, function () use ($request) {
+            $cat = (int) $request->cat;
             $topics = $this->getAllTopics($cat);
             $category = App\Category::all();
             $catName = collect($category)->where('id', $cat);
@@ -41,14 +39,12 @@ class BoardController extends Controller
                 return null;
             }
 
-            return compact('topics', 'category', 'cat', 'catName');
+            return view('board.index', compact('topics', 'category', 'cat', 'catName'))->render();
         });
-
-        if ($data === null) {
+        if ($view === null) {
             return redirect('/board');
         }
-
-        return view('board.index', $data);
+        return $view;
     }
 
     /**
@@ -58,11 +54,10 @@ class BoardController extends Controller
      *
      * @return mixed
      */
-    private function getAllTopics(int $category)
-    {
+    private function getAllTopics(int $category) {
         $topics = App\Topic::with('user', 'category')->orderBy('status', 'desc')->orderBy('updated_at', 'desc')->active();
 
-        if (!empty($category)) {
+        if(!empty($category)) {
             $topics->where('category_id', $category);
         }
 
@@ -72,19 +67,17 @@ class BoardController extends Controller
     /**
      * Show the topic
      *
-     * @param  Request $request
-     * @param  Int $id Topic Id
-     *
+     * @param  Request  $request
+     * @param  Int      $id       Topic Id
      * @return Response
      */
-    public function show(Request $request, $id)
-    {
+    public function show(Request $request, $id) {
 
-        $data = Cache::remember('topic.' . $id, 1, function () use ($request, $id) {
-            $topic = App\Topic::with('comment', 'comment.user', 'category')->where('id', $id)->first();
+        $view = Cache::remember('topic.' . $id, 1, function () use ($request, $id){
+            $topic = App\Topic::with('comment', 'comment.user')->where('id', $id)->first();
 
             if (empty($topic)) {
-                return null;
+                return view('errors.topic404')->render();
             }
 
             $headerTitle = 'Minecraft SkyRack - ' . $topic->title;
@@ -92,16 +85,13 @@ class BoardController extends Controller
 
             $headerDescription = trim(preg_replace('/\s+/', ' ', $headerDescription));
             // Remove &nbsp;
-            $headerDescription = str_replace("\xc2\xa0", ' ', $headerDescription);
+            $headerDescription = str_replace("\xc2\xa0",' ',$headerDescription);
 
-            return compact('topic', 'headerTitle', 'headerDescription');
+            # return compact('topic', 'headerTitle', 'headerDescription');
+            return view('board.show', compact('topic', 'headerTitle', 'headerDescription'))->render();
         });
 
-        if ($data === null) {
-            return redirect()->action('BoardController@index');
-        }
-
-        return view('board.show', $data);
+        return $view;
     }
 
     /**
@@ -109,8 +99,7 @@ class BoardController extends Controller
      *
      * @return Response
      */
-    public function create()
-    {
+    public function create() {
         $category = App\Category::all();
         return view('board.create', ['category' => $category]);
     }
@@ -118,12 +107,10 @@ class BoardController extends Controller
     /**
      * Store the topic
      *
-     * @param  Request $request
-     *
+     * @param  Request  $request
      * @return Response
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         $this->validate($request, [
             'title' => 'required|max:128|min:12',
             'body' => 'required|min:32',
@@ -156,12 +143,10 @@ class BoardController extends Controller
     /**
      * Redirect reply if not have ID topic to reply
      *
-     * @param  Request $request
-     *
+     * @param  Request  $request
      * @return Response
      */
-    public function replyRedirect(Request $request)
-    {
+    public function replyRedirect(Request $request) {
         return redirect()->action('BoardController@index');
     }
 
@@ -172,8 +157,7 @@ class BoardController extends Controller
      * @param int @id
      * @return Response
      */
-    public function reply(Request $request, int $id)
-    {
+    public function reply(Request $request, int $id) {
         $topic = App\Topic::find($id);
 
         if (empty($topic)) {
@@ -189,8 +173,7 @@ class BoardController extends Controller
      * @param Request $request
      * @return Response
      */
-    public function storeReply(Request $request)
-    {
+    public function storeReply(Request $request) {
         $this->validate($request, [
             'body' => 'required|min:8|max:512',
             'id' => 'required',
@@ -222,12 +205,11 @@ class BoardController extends Controller
     /**
      * Show edit topic page
      *
-     * @param  Request $Request
-     * @param  Int $id
+     * @param  Request  $Request
+     * @param  Int      $id
      * @return Response
      */
-    public function edit(Request $request, $id)
-    {
+    public function edit(Request $request, $id) {
         $topic = App\Topic::find($id);
 
         if (empty($topic)) {
@@ -244,12 +226,11 @@ class BoardController extends Controller
     /**
      * Update the topic
      *
-     * @param  Request $request
-     * @param Int $id
+     * @param  Request  $request
+     * @param Int      $id
      * @return Response
      */
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id) {
         $this->validate($request, [
             'title' => 'required|max:128|min:12',
             'body' => 'required|min:32',
@@ -279,12 +260,11 @@ class BoardController extends Controller
     /**
      * Show confirm delete topic page
      *
-     * @param  Request $request
-     * @param  Int $id
+     * @param  Request  $request
+     * @param  Int      $id
      * @return Response
      */
-    public function deleteConfirm(Request $request, $id)
-    {
+    public function deleteConfirm(Request $request, $id) {
         $topic = App\Topic::find($id);
 
         if (empty($topic)) {
@@ -301,12 +281,11 @@ class BoardController extends Controller
     /**
      * Remove topic
      *
-     * @param  Request $request
-     * @param  Int $id
+     * @param  Request  $request
+     * @param  Int      $id
      * @return Response
      */
-    public function destroy(Request $request, $id)
-    {
+    public function destroy(Request $request, $id) {
         $topic = App\Topic::find($id);
 
         if (empty($topic)) {
@@ -327,12 +306,11 @@ class BoardController extends Controller
     /**
      * Toggle pin topic
      *
-     * @param  Request $request
-     * @param  Int $id
+     * @param  Request  $request
+     * @param  Int      $id
      * @return Response
      */
-    public function pin(Request $request, $id)
-    {
+    public function pin(Request $request, $id) {
         $topic = App\Topic::find($id);
 
         if (empty($topic)) {
@@ -353,12 +331,11 @@ class BoardController extends Controller
     /**
      * Toggle lock topic
      *
-     * @param  Request $request
-     * @param  Int $id
+     * @param  Request  $request
+     * @param  Int      $id
      * @return Response
      */
-    public function lock(Request $request, $id)
-    {
+    public function lock(Request $request, $id) {
         $topic = App\Topic::find($id);
 
         if (empty($topic)) {
@@ -376,5 +353,4 @@ class BoardController extends Controller
         return redirect()->action('BoardController@show', ['id' => $topic->id]);
     }
 }
-
 ?>
