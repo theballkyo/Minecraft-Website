a0ba8dfdbf32ea85396ca6fefe1fd91214d241e2
Theballkyo <theballkyo@gmail.com>
2017-07-28 23:41:52 +0700

    Update env

--- src/resources/views/layouts/aironepage-header.blade.php
+++ src/resources/views/layouts/aironepage-header.blade.php
@@ -4,10 +4,10 @@
     <div class="promo-block">
         <div class="container">
             <div class="margin-b-40">
-                <h1 class="promo-block-title">Minecraft SkyRack</h1>
+                <h1 class="promo-block-title">Minecraft {{ env('MC_SV_NAME', 'Local Server') }}</h1>
                 <p class="promo-block-text">เซิฟเวอร์มายคราฟสไตร์คลาสสิค</p>
             </div>
-            <a class="btn-theme btn-theme-md btn-white-bg text-uppercase" href="{{ action('BoardController@show', ['id' => 4]) }}">IP: play.mc-skyrack.tk</a>
+            <a class="btn-theme btn-theme-md btn-white-bg text-uppercase" href="{{ action('BoardController@show', ['id' => 4]) }}">IP:{{ env('MC_HOST', 'localhost') }}</a>
         </div>
     </div>
     <!--========== SLIDER ==========-->
